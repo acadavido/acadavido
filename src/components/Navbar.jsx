22a701137198a9ba8febf448897de56994e75978
Andrea Cadavid <andreacadavidosorio@gmail.com>
2023-04-12 17:06:22 +1000

    test2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a className="flex justify-between items-center w-full text-gray-300" href='https://github.com/acadavido' target='_blank'>
-              GitHu <FaGithub size={30}/>
+              GitHub <FaGithub size={30}/>
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#4b9e8c]'>
