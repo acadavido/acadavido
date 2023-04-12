173d969f891a8d493ef45165d840d4c44d85c330
Andrea Cadavid <andreacadavidosorio@gmail.com>
2023-04-12 14:11:20 +1000

    Change root for pdf

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ const Navbar = () => {
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a className="flex justify-between items-center w-full text-gray-300" href="../src/resume/AndreaCadavid.pdf" download="AndreaCadavid.pdf">
+            <a className="flex justify-between items-center w-full text-gray-300" href="./src/resume/AndreaCadavid.pdf" download="AndreaCadavid">
               Resume <BsFillPersonLinesFill size={30}/>
             </a>
           </li>
